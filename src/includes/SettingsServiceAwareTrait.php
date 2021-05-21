70765af425a91284e7dad70b6df184251ac759de
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-05-22 01:00:19 +0200

    Regression bug fixed

--- src/includes/SettingsServiceAwareTrait.php
+++ src/includes/SettingsServiceAwareTrait.php
@@ -59,4 +59,78 @@ trait SettingsServiceAwareTrait {
 	}
 
 	// endregion
+
+	// region METHODS
+
+	/**
+	 * Wrapper around the service's own method.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $field_id       The ID of the field within the settings to read from the database.
+	 * @param   string  $settings_id    The ID of the settings group to read from the database.
+	 * @param   array   $params         Other parameters required for the adapter to work.
+	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 *
+	 * @return  mixed
+	 */
+	public function get_option_value( string $field_id, string $settings_id, array $params = array(), string $handler_id = 'default' ) {
+		return $this->get_settings_service()->get_option_value( $field_id, $settings_id, $params, $handler_id );
+	}
+
+	/**
+	 * Wrapper around the service's own method.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $field_id       The ID of the field to read from the database.
+	 * @param   mixed   $object_id      The ID of the object the data is for.
+	 * @param   array   $params         Other parameters required for the adapter to work.
+	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 *
+	 * @return  mixed
+	 */
+	public function get_field_value( string $field_id, $object_id, array $params = array(), string $handler_id = 'default' ) {
+		return $this->get_settings_service()->get_field_value( $field_id, $object_id, $params, $handler_id );
+	}
+
+	/**
+	 * Wrapper around the service's own method.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $field_id       The ID of the field within the settings to update.
+	 * @param   mixed   $value          The new value of the setting.
+	 * @param   string  $settings_id    The ID of the settings group to update.
+	 * @param   array   $params         Other parameters required for the adapter to work.
+	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 *
+	 * @return  mixed
+	 */
+	public function update_option_value( string $field_id, $value, string $settings_id, array $params = array(), string $handler_id = 'default' ) {
+		return $this->get_settings_service()->update_option_value( $field_id, $value, $settings_id, $params, $handler_id );
+	}
+
+	/**
+	 * Wrapper around the service's own method.
+	 *
+	 * @since   1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param   string  $field_id       The ID of the field to update.
+	 * @param   mixed   $value          The new value of the setting.
+	 * @param   mixed   $object_id      The ID of the object the update is for.
+	 * @param   array   $params         Other parameters required for the adapter to work.
+	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 *
+	 * @return  mixed
+	 */
+	public function update_field_value( string $field_id, $value, $object_id, array $params = array(), string $handler_id = 'default' ) {
+		return $this->get_settings_service()->update_field_value( $field_id, $value, $object_id, $params, $handler_id );
+	}
+
+	// endregion
 }
