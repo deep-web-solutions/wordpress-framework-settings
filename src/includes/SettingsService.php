f4c9cdfdc8d86069fe21cf71da907c8ed8d48504
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-05-22 00:48:31 +0200

    Bug-fixed the service

--- src/includes/SettingsService.php
+++ src/includes/SettingsService.php
@@ -98,18 +98,18 @@ class SettingsService extends AbstractMultiHandlerService implements HooksServic
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @param   string  $page_title     The text to be displayed in the title tags of the page when the menu is selected.
-	 * @param   string  $menu_title     The text to be used for the menu.
-	 * @param   string  $menu_slug      The slug name to refer to this menu by. Should be unique for this menu page and only
-	 *                                  include lowercase alphanumeric, dashes, and underscores characters to be compatible
-	 *                                  with sanitize_key().
-	 * @param   string  $capability     The capability required for this menu to be displayed to the user.
-	 * @param   array   $params         Other params required for the adapter to work.
-	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 * @param   string|callable     $page_title     The text to be displayed in the title tags of the page when the menu is selected.
+	 * @param   string|callable     $menu_title     The text to be used for the menu.
+	 * @param   string              $menu_slug      The slug name to refer to this menu by. Should be unique for this menu page and only
+	 *                                              include lowercase alphanumeric, dashes, and underscores characters to be compatible
+	 *                                              with sanitize_key().
+	 * @param   string              $capability     The capability required for this menu to be displayed to the user.
+	 * @param   array               $params         Other params required for the adapter to work.
+	 * @param   string              $handler_id     The ID of the settings framework handler to use.
 	 *
 	 * @return  mixed|null
 	 */
-	public function register_menu_page( string $page_title, string $menu_title, string $menu_slug, string $capability, array $params = array(), string $handler_id = 'default' ) {
+	public function register_menu_page( $page_title, $menu_title, string $menu_slug, string $capability, array $params = array(), string $handler_id = 'default' ) {
 		return $this->get_handler( $handler_id )->register_menu_page( $page_title, $menu_title, $menu_slug, $capability, $params );
 	}
 
@@ -119,19 +119,19 @@ class SettingsService extends AbstractMultiHandlerService implements HooksServic
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @param   string  $parent_slug    The slug name for the parent menu (or the file name of a standard WordPress admin page).
-	 * @param   string  $page_title     The text to be displayed in the title tags of the page when the menu is selected.
-	 * @param   string  $menu_title     The text to be used for the menu.
-	 * @param   string  $menu_slug      The slug name to refer to this menu by. Should be unique for this menu page and only
-	 *                                  include lowercase alphanumeric, dashes, and underscores characters to be compatible
-	 *                                  with sanitize_key().
-	 * @param   string  $capability     The capability required for this menu to be displayed to the user.
-	 * @param   array   $params         Other parameters required for the adapter to work.
-	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 * @param   string              $parent_slug    The slug name for the parent menu (or the file name of a standard WordPress admin page).
+	 * @param   string|callable     $page_title     The text to be displayed in the title tags of the page when the menu is selected.
+	 * @param   string|callable     $menu_title     The text to be used for the menu.
+	 * @param   string              $menu_slug      The slug name to refer to this menu by. Should be unique for this menu page and only
+	 *                                              include lowercase alphanumeric, dashes, and underscores characters to be compatible
+	 *                                              with sanitize_key().
+	 * @param   string              $capability     The capability required for this menu to be displayed to the user.
+	 * @param   array               $params         Other parameters required for the adapter to work.
+	 * @param   string              $handler_id     The ID of the settings framework handler to use.
 	 *
 	 * @return  mixed|null
 	 */
-	public function register_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $menu_slug, string $capability, array $params = array(), string $handler_id = 'default' ) {
+	public function register_submenu_page( string $parent_slug, $page_title, $menu_title, string $menu_slug, string $capability, array $params = array(), string $handler_id = 'default' ) {
 		return $this->get_handler( $handler_id )->register_submenu_page( $parent_slug, $page_title, $menu_title, $menu_slug, $capability, $params );
 	}
 
@@ -141,16 +141,16 @@ class SettingsService extends AbstractMultiHandlerService implements HooksServic
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @param   string  $group_id       The ID of the settings group.
-	 * @param   string  $group_title    The title of the settings group.
-	 * @param   array   $fields         The fields to be registered with the group.
-	 * @param   string  $page           The settings page on which the group's fields should be displayed.
-	 * @param   array   $params         Other parameters required for the adapter to work.
-	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 * @param   string              $group_id       The ID of the settings group.
+	 * @param   string|callable     $group_title    The title of the settings group.
+	 * @param   array               $fields         The fields to be registered with the group.
+	 * @param   string              $page           The settings page on which the group's fields should be displayed.
+	 * @param   array               $params         Other parameters required for the adapter to work.
+	 * @param   string              $handler_id     The ID of the settings framework handler to use.
 	 *
 	 * @return  mixed|null
 	 */
-	public function register_options_group( string $group_id, string $group_title, array $fields, string $page, array $params = array(), string $handler_id = 'default' ) {
+	public function register_options_group( string $group_id, $group_title, array $fields, string $page, array $params = array(), string $handler_id = 'default' ) {
 		return $this->get_handler( $handler_id )->register_options_group( $group_id, $group_title, $fields, $page, $params );
 	}
 
@@ -160,16 +160,16 @@ class SettingsService extends AbstractMultiHandlerService implements HooksServic
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @param   string  $group_id       The ID of the settings group.
-	 * @param   string  $group_title    The title of the settings group.
-	 * @param   array   $fields         The fields to be registered with the group.
-	 * @param   array   $locations      Where the group should be outputted.
-	 * @param   array   $params         Other parameters required for the adapter to work.
-	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 * @param   string              $group_id       The ID of the settings group.
+	 * @param   string|callable     $group_title    The title of the settings group.
+	 * @param   array               $fields         The fields to be registered with the group.
+	 * @param   array               $locations      Where the group should be outputted.
+	 * @param   array               $params         Other parameters required for the adapter to work.
+	 * @param   string              $handler_id     The ID of the settings framework handler to use.
 	 *
 	 * @return  mixed|null
 	 */
-	public function register_generic_group( string $group_id, string $group_title, array $fields, array $locations, array $params = array(), string $handler_id = 'default' ) {
+	public function register_generic_group( string $group_id, $group_title, array $fields, array $locations, array $params = array(), string $handler_id = 'default' ) {
 		return $this->get_handler( $handler_id )->register_generic_group( $group_id, $group_title, $fields, $locations, $params );
 	}
 
@@ -179,16 +179,16 @@ class SettingsService extends AbstractMultiHandlerService implements HooksServic
 	 * @since   1.0.0
 	 * @version 1.0.0
 	 *
-	 * @param   string  $group_id       The ID of the parent group that the dynamically added field belongs to.
-	 * @param   string  $field_id       The ID of the newly registered field.
-	 * @param   string  $field_title    The title of the newly registered field.
-	 * @param   string  $field_type     The type of custom field being registered.
-	 * @param   array   $params         Other parameters required for the adapter to work.
-	 * @param   string  $handler_id     The ID of the settings framework handler to use.
+	 * @param   string              $group_id       The ID of the parent group that the dynamically added field belongs to.
+	 * @param   string              $field_id       The ID of the newly registered field.
+	 * @param   string|callable     $field_title    The title of the newly registered field.
+	 * @param   string              $field_type     The type of custom field being registered.
+	 * @param   array               $params         Other parameters required for the adapter to work.
+	 * @param   string              $handler_id     The ID of the settings framework handler to use.
 	 *
 	 * @return  mixed|null
 	 */
-	public function register_field( string $group_id, string $field_id, string $field_title, string $field_type, array $params = array(), string $handler_id = 'default' ) {
+	public function register_field( string $group_id, string $field_id, $field_title, string $field_type, array $params = array(), string $handler_id = 'default' ) {
 		return $this->get_handler( $handler_id )->register_field( $group_id, $field_id, $field_title, $field_type, $params );
 	}
 
